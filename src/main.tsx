dc683d5bda34b2cf9e4e38967fb542bcd07def7e
Atabek <atabek122002@gmail.com>
2025-02-06 15:44:12 +0500

    Добавил аналитику

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -8,5 +9,6 @@ import 'src/assets/styles/index.css';
 createRoot(document.getElementById('root')!).render(
   <Router>
     <App />
+    <Analytics />
   </Router>,
 );
